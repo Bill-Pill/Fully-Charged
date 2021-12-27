65810682471684dd4763a4a32b4df24cddfe38a6
Bill-Pill <delashmuttwa@gmail.com>
2021-12-27 09:59:30 -0500

    Misc. format/verbage changes.
    Updated primary color to match logo.  Change 'Project' to 'Exercise'.  Updated code format/comments for better readability.

--- js/site.js
+++ js/site.js
@@ -5,42 +5,37 @@ function getValues() {
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
 
-    // Input validation
     // Parse input into integers
     startValue = parseInt(startValue);
     endValue = parseInt(endValue);
 
+    // Validate inputs then continue
     if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
         // Call generateNumbers
         let numbers = generateNumbers(startValue, endValue);
         // Call displayNumbers
         displayNumbers(numbers);
-
     } else {
         alert("You must enter integers");
     }
-
-    
 }
 
 // Generate numbers fom startValue to endValue
 // Logic function(s)
 function generateNumbers(sValue, eValue) {
-
     let numbers = [];
 
     // Get numbers from start to finish
     for (let i = sValue; i <= eValue; i++) {
         numbers.push(i);
     }
-
     return numbers;
 }
 
 // Display the numbers and mark evens as bold
 // Display/view functions
 function displayNumbers(numbers) {
-
+    // Variable to hold HTML
     let templateRows = "";
 
     for (let index = 0; index < numbers.length; index++) {
@@ -48,14 +43,14 @@ function displayNumbers(numbers) {
         let className = "even";
         let number = numbers[index];
 
-        if(number % 2 === 0) {
+        // Checks if number is even
+        if (number % 2 === 0) {
             className = "even";
-        }
-        else {
+        } else {
             className = "odd"
         }
+
         templateRows += `<tr><td class="${className}">${number}</td></tr>`;
     }
-
     document.getElementById("results").innerHTML = templateRows;
 }
